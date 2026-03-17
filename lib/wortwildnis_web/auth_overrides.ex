02df042dd406a99ba4f9485fb9da5fa3949a3283
Jan Wirth <jottenwe@gmail.com>
2026-03-16 20:58:58 -0600

    snapshot

--- lib/wortwildnis_web/auth_overrides.ex
+++ lib/wortwildnis_web/auth_overrides.ex
@@ -10,4 +10,19 @@ defmodule WortwildnisWeb.AuthOverrides do
     set :register_form_module, WortwildnisWeb.Auth.RegisterFormWithHcaptcha
     set :reset_form_module, WortwildnisWeb.Auth.ResetFormWithHcaptcha
   end
+
+  override AshAuthentication.Phoenix.Components.Password.Input do
+    set :submit_class,
+        "btn btn-block mt-4 mb-4 bg-blue-600 text-white border-none hover:opacity-90 disabled:opacity-50"
+  end
+
+  override AshAuthentication.Phoenix.Components.Confirm.Input do
+    set :submit_class,
+        "btn btn-block mt-4 mb-4 bg-blue-600 text-white border-none hover:opacity-90 disabled:opacity-50"
+  end
+
+  override AshAuthentication.Phoenix.Components.MagicLink.Input do
+    set :submit_class,
+        "btn btn-block mt-4 mb-4 bg-blue-600 text-white border-none hover:opacity-90 disabled:opacity-50"
+  end
 end
